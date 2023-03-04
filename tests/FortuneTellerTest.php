b0c90b1f0b27edf880532e095e5d1782b26fdabe
melissagerber <10280898@uvu.edu>
2023-03-04 13:07:59 -0700

    first commit

---
+++ tests/FortuneTellerTest.php
@@ -0,0 +1,18 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require __DIR__ . "/../src/FortuneTeller.php";
+
+class FortuneTellerTest extends TestCase {
+
+
+   public function testRelationshipPrediction_WithDefaultEntries_ReturnsNotNull() {
+
+   }
+
+   public function testMoneyArray_WithDefaultEntires_ReturnsCountOf3() {
+
+   }
+
+   
+}
